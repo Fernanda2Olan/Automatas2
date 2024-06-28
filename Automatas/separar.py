462f4c0f4290e9dec00f62588b834cdbc8c468a4
Fernanda Olan <95500991+Fernanda2Olan@users.noreply.github.com>
2024-06-28 12:04:25 -0700

    Add files via upload

--- Automatas/separar.py
+++ Automatas/separar.py
@@ -1,8 +1,15 @@
+#Validación de caracteres en la gramatica latina
+
+import string
+
+def gramaticass():
+    return "abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ0123456789!/,.-_'?*:;?{}[]"
+
 def separar_por_caracteres(cadena):
-    caracteres = []
-    for caracter in cadena:
-        caracteres.append(caracter)
-    return caracteres
+    return [char for char in cadena]
+
+def es_caracter_valido(caracter):
+    return caracter in gramaticass()
 
 cadena_usuario = input("Ingrese una cadena de texto: ")
 
