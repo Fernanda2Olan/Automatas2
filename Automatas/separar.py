2c6333279b58eb58428e9314198b7cd7de86e00a
Fernanda Olan <95500991+Fernanda2Olan@users.noreply.github.com>
2024-07-02 12:56:59 -0700

    Caracteres validos

--- Automatas/separar.py
+++ Automatas/separar.py
@@ -1,15 +1,17 @@
 #Validación de caracteres en la gramatica latina
 
-import string
-
-def gramaticass():
-    return "abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ0123456789!/,.-_'?*:;?{}[]"
+caracteres = {'a', 'á', 'b', 'c', 'd', 'e', 'é', 'f', 'g', 'h', 'i', 'í', 'j',
+              'k', 'l', 'm', 'n', 'ñ', 'o', 'ó', 'p', 'q', 'r', 's', 't',
+              'u', 'ú', 'ü', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4',
+              '5', '6', '7', '8', '9', ',', '.', ';', ':', '!', '¡', '¿', '?',
+              '-', ' '}
 
 def separar_por_caracteres(cadena):
-    return [char for char in cadena]
-
-def es_caracter_valido(caracter):
-    return caracter in gramaticass()
+    cadena = cadena.lower()
+    for caracters in cadena:
+        if caracters not in caracteres:
+            return f"Error, el carácter es invalido {caracters}"    
+    return list (cadena)
 
 cadena_usuario = input("Ingrese una cadena de texto: ")
 
